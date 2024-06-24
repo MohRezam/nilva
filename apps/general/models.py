872799655c65aae2c84bed61f31415558de4d78d
MohRezam <mkalhor81126@gmail.com>
2024-06-24 15:01:42 +0330

    Add str method to CurrentData

--- apps/general/models.py
+++ apps/general/models.py
@@ -17,14 +17,15 @@ class CurrentData(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     
-    
+    def __str__(self) -> str:
+        return f"{self.location_name}:{self.latitude}--{self.longitude}"
 
 class FutureHour(models.Model):
     weather_data = models.ForeignKey(CurrentData, related_name='future_hours', on_delete=models.CASCADE)
     date = models.BigIntegerField()
     temp = models.FloatField()
     weather_icon_type = models.CharField(max_length=255)
-
+    
 class FutureDay(models.Model):
     weather_data = models.ForeignKey(CurrentData, related_name='future_days', on_delete=models.CASCADE)
     date = models.BigIntegerField()
