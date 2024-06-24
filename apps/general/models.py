c3be04bd64a801082dc81442fece1b26635739b2
MohRezam <mkalhor81126@gmail.com>
2024-06-24 14:39:20 +0330

    Add models to the project

---
+++ apps/general/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class CurrentData(models.Model):
+    date = models.BigIntegerField()
+    location_name = models.CharField(max_length=255)
+    temp_now = models.FloatField()
+    temp_min = models.FloatField()
+    temp_max = models.FloatField()
+    weather_icon_type = models.CharField(max_length=255)
+    wind_speed = models.FloatField()
+    wind_deg = models.FloatField()
+    humidity = models.IntegerField()
+    pressure = models.IntegerField()
+    sunrise = models.BigIntegerField()
+    sunset = models.BigIntegerField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    
+    
+
+class FutureHour(models.Model):
+    weather_data = models.ForeignKey(CurrentData, related_name='future_hours', on_delete=models.CASCADE)
+    date = models.BigIntegerField()
+    temp = models.FloatField()
+    weather_icon_type = models.CharField(max_length=255)
+
+class FutureDay(models.Model):
+    weather_data = models.ForeignKey(CurrentData, related_name='future_days', on_delete=models.CASCADE)
+    date = models.BigIntegerField()
+    temp_min = models.FloatField()
+    temp_max = models.FloatField()
+    weather_icon_type = models.CharField(max_length=255)
+    
+    
