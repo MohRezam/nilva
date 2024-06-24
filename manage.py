5d2ebc7068e994a879bbeccfc30b365ef3367272
MohRezam <mkalhor81126@gmail.com>
2024-06-24 14:42:29 +0330

    Modify DJANGO_SETTINGS_MODULE address

--- manage.py
+++ manage.py
@@ -3,12 +3,10 @@
 import os
 import sys
 
-from core_weather_backend.settings.base import DJANGO_SETTINGS_MODULE
-
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_weather_backend.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_weather_backend.settings.components')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
